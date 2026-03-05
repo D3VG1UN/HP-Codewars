691a58e2ab6208c4256b67f51a244a8fc709b891
EricCasals <eric.casals@campus.xaloc.org>
2026-03-05 21:09:05 +0100

    Práctica concurso HP

---
+++ PrácticaDiccionario.py
@@ -0,0 +1,4 @@
+diccionario=dict(name="Juan",edad=33,caca="5 veces a la semana")
+
+print(diccionario)
+print(diccionario["edad"])
--- Prácticas concurso HP/2025/Ej6.py
+++ Prácticas concurso HP/2025/Ej6.py
@@ -1,11 +1,5 @@
-notas=""
-cantidadnotas=int(input("Cuántas notas? "))
-dec=int(input("Introduce los decimales: "))
-
-
-for i in range (cantidadnotas):
-    nota=float(input("Introduce la nota: "))
-    nota=round(nota, dec)
-    notas= str(notas) + " " + str(nota)
-
-print(notas[1:])
+nums=list(input().split())
+decimals=int(input())
+totalnums=0
+for i in range (len(nums)):
+    totalnums+=1
